086553a69a5964b41179498b2a9fee6cdbdc627d
Simonel David <simonel.david@stud.ubbcluj.ro>
2024-05-02 17:39:04 +0300

    got back from mongodb

--- backend/src/main/java/com/example/ClimatePerception/AuthController.java
+++ backend/src/main/java/com/example/ClimatePerception/AuthController.java
@@ -1,33 +1,44 @@
 package com.example.ClimatePerception;
 
+import com.example.ClimatePerception.repository.csv.FileCsvHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 import com.example.ClimatePerception.model.User;
-import com.example.ClimatePerception.service.UserService;
 
 @RestController
 @CrossOrigin(origins = "https://heat.island.aim-space.com")
 public class AuthController {
-    private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+    private final String csvFilePath = "src/main/resources/users.csv";
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
         String email = credentials.get("email");
         String password = credentials.get("password");
 
-        User user = userService.findByEmailAndPassword(email, password);
-        if (user != null) {
+        if (verifierAuthenticate(email, password)) {
             return ResponseEntity.ok("Authentication successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect login credentials");
         }
+    }
+
+    private boolean verifierAuthenticate(String username, String password) {
+        List<User> users = FileCsvHandler.readUsersFromCsv();
+
+        for (User user : users) {
+            if (user.getEmail().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
