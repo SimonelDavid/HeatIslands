77d50a93617078e7d9bac81a3c28c8b12555dff6
Simonel David <simonel.david@stud.ubbcluj.ro>
2024-05-18 19:51:32 +0300

    removed metrics from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,16 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(metric => {
-  fetch('http://backend:8080/metrics', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(metric)
-  });
-});
